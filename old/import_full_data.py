ff8c67ebbae1ff2279b1c2abed3d91d8ee89e7ef
Zachary Stence <zachstence@gmail.com>
2018-07-05 14:28:33 -0400

    Change nperseg to sqrt
    - Change nperseg parameter of energy, power, and coherence calculation to be sqrt(signal length) as recommended by another source
    - Change coherence fs parameter to be 1/(dt * w) since calculating coherence of features, not signal samples
    - Create new datasets with the above parameters changed
    - Test with those new datasets, similar results (however I feel these are better parameters and make more sense)

--- old/import_full_data.py
+++ old/import_full_data.py
@@ -24,26 +24,6 @@ import matplotlib.pyplot as plt
       # weight      - an unsiged 8-bit integer
 ########################################################################################################
 
-# given the accel_data for a particular person and a list of desired activities, extract all data
-def getAccelData(accel_data):
-  total = 0
-  count = 0
-  # for act_index in range(len(accel_data)):
-  act_index = 2
-  activity = accel_data[act_index]
-  for trial_index in range(len(activity)):
-    trial = activity[trial_index][0]
-    x_accel          = trial[0]
-    y_accel          = trial[1]
-    z_accel          = trial[2]
-    time_nanoseconds = trial[3]
-    time_seconds     = trial[4]
-    magnitude        = trial[5]
-    total += len(magnitude)
-    count += 1
-    # At this point all fields are in variables for later processing
-  print("average = {}".format(total / count))
-
 # List of activities in dataset
 ACTIVITIES = ['StandingUpFS',
               'StandingUpFL',
@@ -64,25 +44,23 @@ ACTIVITIES = ['StandingUpFS',
               'FallingLeft']
 
 # Import MatLab data as a dictionary
-mat = scipy.loadmat('./data/UniMiB-SHAR/data/full_data.mat')
+mat = scipy.loadmat('../data/UniMiB-SHAR/data/full_data.mat')
 
 # Get numpy arrays containing data
 data = mat['full_data']
 
-# Loop through data and extract all fields into variables
-# for i in range(len(data)):
-#   accel_data = data[i][0][0][0] # because each is wrapped in a 2D array
-#   gender     = data[i][1][0][0]
-#   age        = data[i][2][0][0]
-#   height     = data[i][3][0][0]
-#   weight     = data[i][4][0][0]
-#   # for act_index in range(len(accel_data)):
-#   act_index = 3
-#   activity = accel_data[act_index]
-#   print(len(activity))
+total = 0
+for p in range(len(data)):
+  if p == 19:
+    continue
+  accel_data = data[p][0][0][0]
+  a = 2
+  activity = accel_data[a]
+  for t in range(len(activity)):
+    trial = activity[t][0]
+    m = trial[5]
+    print('person {} trial {} len {}'.format(p, t, len(m)))
+    total += len(m)
 
-p = 0
-accel_data = data[p][0][0][0]
-for act_index in range(len(accel_data)):
-  activity = accel_data[act_index]
-  print("{} : {}".format(ACTIVITIES[act_index], len(activity)))
+avg = total / 58
+print(avg)
